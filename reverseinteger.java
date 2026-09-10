f01fa9b468f2ae64d348d623e42ece0c966e1377
Nithin16 <nithiin123456@gmail.com>
2026-09-11 05:00:31 +0530

    Update reverseinteger.java

--- reverseinteger.java
+++ reverseinteger.java
@@ -1,15 +1,18 @@
 class Solution {
     public int reverse(int x) {
 
-        int rev = 0;
+        long rev = 0;
 
         while (x != 0) {
-
             int digit = x % 10;
             rev = rev * 10 + digit;
             x = x / 10;
         }
 
-        return rev;
+        if (rev > 2147483647 || rev < -2147483648L) {
+            return 0;
+        }
+
+        return (int) rev;
     }
 }
